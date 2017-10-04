7154c3f4547ba65079ead6ced07c355ec026c5b7
Tariq <Tshaikh92@gmail.com>
2017-10-03 21:00:19 -0400

    started script + ajax

---
+++ assets/js/sportgatorjs.js
@@ -0,0 +1,8 @@
+
+var queryURL = "api.openweathermap.org/data/2.5/weather?q="+x+"api_key=&APPID=742f986956768279a272daad0a875326"
+
+$('button').on('click', function(){
+	var  = $(this).text
+
+	console.log(queryURL);
+})
